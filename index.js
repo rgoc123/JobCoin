809f9e9c4520e555ee77a16a3264edb2c5dc948a
Robert O'Connor <robertgoconnor2@gmail.com>
2021-01-25 18:41:57 -0500

    Refactor: create separate mixer, separate functions

--- index.js
+++ index.js
@@ -1,77 +1,13 @@
 const apiClient = require('./apiClient.js')
 const utils = require('./utils.js')
+const mixer = require('./mixer.js')
 
 const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout
 })
 
-const mixAndDistribute = (addresses, amount) => {
-  // TODO: Restore below
-  // const addressesArray = addresses.split(',')
-  const addressesArray = ['1', '2', '3', '4', '5']
-
-  const subAmounts = []
-  let remainingAmount = amount
-
-  // Make intial slices of amount to send
-  while (remainingAmount) {
-    if (remainingAmount < 5) {
-      subAmounts.push(remainingAmount)
-      remainingAmount = 0
-    } else {
-      const subAmount = Math.floor(Math.random() * (remainingAmount) + 1)
-      subAmounts.push(subAmount)
-
-      remainingAmount -= subAmount
-    }
-  }
-
-  // Make sure subAmounts length is at least num addresses
-  if (subAmounts.length < addressesArray.length) {
-    console.log('splitting')
-    while (subAmounts.length < addressesArray.length) {
-      for (let idx in subAmounts) {
-        const subAmount = subAmounts[idx]
-        if (subAmount > 1) {
-          const origAmount = subAmounts.splice(idx, 1)
-          const newAmountOne = Math.floor(Math.random() * origAmount) + 1
-          const newAmountTwo = origAmount - newAmountOne
-
-          subAmounts.push(newAmountOne)
-          subAmounts.push(newAmountTwo)
-          break
-        }
-      }
-    }
-  }
-
-  console.log('FINAL SUBAMOUNTS')
-  console.log(subAmounts)
-
-  for (let idx in subAmounts) {
-    const subAmount = subAmounts[idx]
-    // Create random time anywhere between 1 and 6 seconds
-    const randomTimeInterval = Math.floor(Math.random() * 5 + 1)
-
-    if (idx < addressesArray.length) { // Make sure each addr gets one subAmount
-      const toAddress = addressesArray[idx]
-
-      setTimeout(() => {
-        apiClient.makeTx('HOUSE', toAddress, subAmount)
-      }, randomTimeInterval)
-    } else { // Once every addr has gotten a subAmount, randomly distribute
-      const randomAddressIdx = Math.floor(Math.random() * addressesArray.length)
-      const toAddress = addressesArray[randomAddressIdx]
-
-      setTimeout(() => {
-        apiClient.makeTx('HOUSE', toAddress, subAmount)
-      }, randomTimeInterval)
-    }
-  }
-}
-
-mixAndDistribute('', 30)
+mixer.mixAndDistribute('', 30)
 
 const askCoinAmount = (addresses) => {
   readline.question('How many coins would you like to send? ', async (amount) => {
---
+++ mixer.js
@@ -0,0 +1,76 @@
+const createRandomAmounts = (amount, addressesArray) => {
+  const subAmounts = []
+
+  let remainingAmount = amount
+  const numAddresses = addressesArray.length
+
+  // Make intial slices of amount to send
+  while (remainingAmount) {
+    if (remainingAmount < 5) {
+      subAmounts.push(remainingAmount)
+      remainingAmount = 0
+    } else {
+      const subAmount = Math.floor(Math.random() * (remainingAmount) + 1)
+      subAmounts.push(subAmount)
+
+      remainingAmount -= subAmount
+    }
+  }
+
+  // Make sure subAmounts length is at least num addresses
+  if (subAmounts.length < numAddresses) {
+    console.log('splitting')
+    while (subAmounts.length < numAddresses) {
+      for (let idx in subAmounts) {
+        const subAmount = subAmounts[idx]
+        if (subAmount > 1) {
+          const origAmount = subAmounts.splice(idx, 1)
+          const newAmountOne = Math.floor(Math.random() * origAmount) + 1
+          const newAmountTwo = origAmount - newAmountOne
+
+          subAmounts.push(newAmountOne)
+          subAmounts.push(newAmountTwo)
+          break
+        }
+      }
+    }
+  }
+  
+  console.log('FINAL SUBAMOUNTS')
+  console.log(subAmounts)
+
+  return subAmounts
+}
+
+const distribute = (subAmounts, addressesArray) => {
+  for (let idx in subAmounts) {
+    const subAmount = subAmounts[idx]
+    // Create random time anywhere between 1 and 6 seconds
+    const randomTimeInterval = Math.floor(Math.random() * 5 + 1)
+
+    if (idx < addressesArray.length) { // Make sure each addr gets one subAmount
+      const toAddress = addressesArray[idx]
+
+      // setTimeout(() => {
+      //   apiClient.makeTx('HOUSE', toAddress, subAmount)
+      // }, randomTimeInterval)
+    } else { // Once every addr has gotten a subAmount, randomly distribute
+      const randomAddressIdx = Math.floor(Math.random() * addressesArray.length)
+      const toAddress = addressesArray[randomAddressIdx]
+
+      // setTimeout(() => {
+      //   apiClient.makeTx('HOUSE', toAddress, subAmount)
+      // }, randomTimeInterval)
+    }
+  }
+}
+
+exports.mixAndDistribute = (addresses, amount) => {
+  // TODO: Restore below
+  // const addressesArray = addresses.split(',')
+  const addressesArray = ['1', '2', '3', '4', '5']
+
+  const subAmounts = createRandomAmounts(amount, addressesArray)
+
+  distribute(subAmounts, addressesArray)
+}
