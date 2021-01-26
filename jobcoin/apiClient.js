7245147a8c195252b27796e5b9eed50daca324b1
Robert O'Connor <robertgoconnor2@gmail.com>
2021-01-26 00:54:30 -0500

    Refactor: remove unnecessary function

--- jobcoin/apiClient.js
+++ jobcoin/apiClient.js
@@ -7,18 +7,6 @@ const API_BASE_URL = "http://jobcoin.gemini.com/jester-bless/api";
 const API_ADDRESS_URL = `${API_BASE_URL}/addresses`;
 const API_TRANSACTIONS_URL = `${API_BASE_URL}/transactions`;
 
-exports.getTxs = async () => {
-  try {
-    const addressesRes = await axios.get(API_TRANSACTIONS_URL)
-    console.log('RES DATA')
-    console.log(addressesRes.data)
-    return
-  } catch (err) {
-    console.log('ERROR')
-    console.log(err)
-  }
-}
-
 exports.makeTx  = async (fromAddress, toAddress, amount) => {
   try {
     const tx = await axios.post(API_TRANSACTIONS_URL, {
